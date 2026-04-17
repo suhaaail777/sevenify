6cca11d0fcaa8f01b116443ddc150d4521a7e021
suhaaail <suhaaail777@gmail.com>
2026-04-17 22:55:15 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'sevenify-v1';
+const ASSETS = ['./', './index.html', './applogo.png', './sw.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
